65db48272bab73c20e0efe7e2d29f4324f102ad0
gnomeunity <gnomeunity@gmail.com>
2026-09-05 16:41:23 +0545

    adding init file

---
+++ script.js
@@ -0,0 +1,5 @@
+const toggle=document.querySelector(".menu-toggle"),links=document.querySelector(".nav-links");
+toggle.addEventListener("click",()=>links.classList.toggle("open"));
+document.querySelectorAll(".nav-links a").forEach(a=>a.addEventListener("click",()=>links.classList.remove("open")));
+document.getElementById("year").textContent=new Date().getFullYear();
+document.querySelectorAll('a[href="#"]').forEach(a=>a.addEventListener("click",e=>e.preventDefault()));
